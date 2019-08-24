ebb7ec09e306c16bbed12620b2d089da0db79d05
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2019-08-24 13:06:59 -0700

    Fix consistency issues and minor revamp

--- StratBuild.py
+++ StratBuild.py
@@ -6,31 +6,46 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d","--description", nargs='?', help="Strat description")
 parser.add_argument("-s","--signature", nargs='?', help="Developer signature")
 parser.add_argument("-p","--path", nargs='?', help="Build path")
-
-parser.add_argument("block0", help="Segment 0")
-parser.add_argument("block1", help="Segment 1")
-parser.add_argument("-U0","--UC00", nargs='?', help="Add uncompressed segment 0 to strat")
-parser.add_argument("-U1","--UC01", nargs='?', help="Add uncompressed segment 1 to strat")
-
+parser.add_argument("C00", help="Segment 00")
+parser.add_argument("C01", help="Segment 01")
+parser.add_argument("-U0","--UC00", nargs='?', help="Uncompressed segment 00")
+parser.add_argument("-U1","--UC01", nargs='?', help="Uncompressed segment 01")
 parser.add_argument("-pc","--pcstrat", action= "store_true", help="Do not attach uncompressed segments to strat")
 args = parser.parse_args()
 
+def getSize(filename):
+    f2 = open(filename,"rb")
+    f2.seek(0x00, os.SEEK_END)
+    size = f2.tell()
+    f2.close()
+    return size
 
-if args.signature is None:
-    raise ValueError("Please input a signature")
+def getLiteralSize(filesize):
+    literalSize = (filesize / 0x04) * (0x05)
+    return literalSize
 
-if args.path is None:
-    raise ValueError("Please input a build path")
+def literalComp(filename, filesize):
+    f2 = open(filename,"rb")
+    loopsize = (filesize / 0x04)
+    for i in range(0,loopsize):
+        literalBytes = struct.unpack('i', f2.read(4))[0]
+        f1.write(bytearray([0x03]))
+        f1.write(struct.pack("i", literalBytes))
+    f2.close()
 
-f0 = open(args.block0,"rb")
-f2 = open(args.block1,"rb")
-if args.UC00 is not None:
-    f3 = open(args.UC00,"rb")
-if args.UC01 is not None:
-    f4 = open(args.UC01,"rb")
+def uncompHandle(filename, filesize):
+    uncompressed_alignment = 0x800
+    f2 = open(filename,"rb")
+    filebytes = f2.read(filesize)
+    data_offset = f1.tell()
+    new_offset = (data_offset + uncompressed_alignment - 1) / uncompressed_alignment * uncompressed_alignment
+    paddsize = new_offset - data_offset
+    f1.write(bytearray([0]*paddsize))
+    f1.write(filebytes)
+    f2.close()
+    return paddsize
+
 f1 = open("out","wb")
-
-uncompressed_alignment = 0x800
 
 fullsize = 0x190
 
@@ -43,135 +58,79 @@ f1.write(struct.pack("i", 0x01))
 
 if args.description is not None:
     f1.write(args.description)
-    argSize = len(args.description)
-    descriptionPadding = 0x40 - argSize
+    descriptionPadding = 0x40 - len(args.description)
     f1.write(bytearray([0])*descriptionPadding)
 else:
     f1.write("<Strat Wad>")
     f1.write(bytearray([0])*0x35)
 
-f1.write(args.signature)
-argSize = len(args.signature)
-signaturePadding = 0x28 - argSize
-f1.write(bytearray([0])*signaturePadding)
+if args.signature is not None:
+    f1.write(args.signature)
+    signaturePadding = 0x28 - len(args.signature)
+    f1.write(bytearray([0])*signaturePadding)
+else:
+    f1.write("normal")
+    f1.write(bytearray([0])*0x22)
+
 
 if args.UC00 is not None:
 
-    f3.seek(0x00, os.SEEK_END)
-    sizeBlockUC00 = f3.tell()
-    f3.seek(0x00, os.SEEK_SET)
-
-    f1.write(struct.pack("i", sizeBlockUC00))
+    sizeUC00 = getSize(args.UC00)
+    f1.write(struct.pack("i", sizeUC00))
 
 else:
     f1.write(struct.pack("i", 0x00))
 
 if args.UC01 is not None:
 
-    f4.seek(0x00, os.SEEK_END)
-    sizeBlockUC01 = f4.tell()
-    f4.seek(0x00, os.SEEK_SET)
-
-    f1.write(struct.pack("i", sizeBlockUC01))
+    sizeUC01 = getSize(args.UC01)
+    f1.write(struct.pack("i", sizeUC01))
 
 else:
     f1.write(struct.pack("i", 0x00))
 
+size00 = getSize(args.C00)
+f1.write(struct.pack("i", size00))
 
-f0.seek(0x00, os.SEEK_END)
-sizeBlock0 = f0.tell()
-f0.seek(0x00, os.SEEK_SET)
+size01 = getSize(args.C01)
+f1.write(struct.pack("i", size01))
 
-f1.write(struct.pack("i", sizeBlock0))
+literalSize00 = getLiteralSize(size00)
+literalSize01 = getLiteralSize(size01)
 
+f1.write(struct.pack("i", literalSize00))
+f1.write(struct.pack("i", literalSize01))
 
-f2.seek(0x00, os.SEEK_END)
-sizeBlock1 = f2.tell()
-f2.seek(0x00, os.SEEK_SET)
+fullsize += (literalSize00 + literalSize01)
 
-f1.write(struct.pack("i", sizeBlock1))
-
-
-literalSize0 = (sizeBlock0 / 0x04) * (0x05)
-
-literalSize1 = (sizeBlock1 / 0x04) * (0x05)
-
-f1.write(struct.pack("i", literalSize0))
-f1.write(struct.pack("i", literalSize1))
-
-fullsize += (literalSize0 + literalSize1)
-
-f1.write(args.path)
-argSize = len(args.path)
-pathPadding = 0x100 - argSize
-f1.write(bytearray([0])*pathPadding)
-
-
-# Block 0
-
-print "Converting Block 0"
-
-loopsize = (sizeBlock0 / 0x04)
-
-for i in range(0,loopsize):
-
-    bytes = struct.unpack('i', f0.read(4))[0]
-
-    f1.write(bytearray([0x03]))
-    f1.write(struct.pack("i", bytes))
-
-
-# Block 1
-
-print "Converting Block 1"
-
-loopsize = (sizeBlock1 / 0x04)
-
-for i in range(0,loopsize):
-
-    bytes = struct.unpack('i', f2.read(4))[0]
-
-    f1.write(bytearray([0x03]))
-    f1.write(struct.pack("i", bytes))
-
-if args.pcstrat == True:
-    print "Skipping attachment of UC blocks"
+if args.path is not None:
+    f1.write(args.path)
+    pathPadding = 0x100 - len(args.path)
+    f1.write(bytearray([0])*pathPadding)
 
 else:
+    currentDirectory = os.getcwd()
+    f1.write(currentDirectory)
+    pathPadding = 0x100 - len(currentDirectory)
+    f1.write(bytearray([0])*pathPadding)
 
-# Block UC00
+print "Converting segment 00"
+literalComp(args.C00, size00)
 
+print "Converting segment 01"
+literalComp(args.C01, size01)
+
+if args.pcstrat == True:
+    print "Skipping attachment of uncompressed segments"
+
+else:
     if args.UC00 is not None:
-        filebytes = f3.read(sizeBlockUC00)
-        data_offset = f1.tell()
-        data_offset2 = (data_offset + uncompressed_alignment - 1) / uncompressed_alignment * uncompressed_alignment
-        paddsize = data_offset2 - data_offset
-
-        print "Padding UC Block 0"
-
-        f1.write(bytearray([0]*paddsize))
-
-        print "Attaching UC Block 0"
-
-        f1.write(filebytes)
-        fullsize += sizeBlockUC00 + paddsize
-
-# Block UC01
+        print "Attaching uncompressed segment 00"
+        fullsize += sizeUC00 + uncompHandle(args.UC00, sizeUC00)
 
     if args.UC01 is not None:
-        filebytes = f4.read(sizeBlockUC01)
-        data_offset = f1.tell()
-        data_offset2 = (data_offset + uncompressed_alignment - 1) / uncompressed_alignment * uncompressed_alignment
-        paddsize = data_offset2 - data_offset
-
-        print "Padding UC Block 1"
-
-        f1.write(bytearray([0]*paddsize))
-        f1.write(filebytes)
-
-        print "Attaching UC Block 1"
-
-        fullsize += sizeBlockUC01 + paddsize
+        print "Attaching uncompressed segment 01"
+        fullsize += sizeUC01 + uncompHandle(args.UC01, sizeUC01)
 
 # handle end padding
 
@@ -182,13 +141,5 @@ while fullsize % 0x1000 != 0x00:
     fullsize += 0x01
 
 print "Done!"
-
-
-f0.close()
-f2.close()
-if args.UC00 is not None:
-    f3.close()
-if args.UC01 is not None:
-    f4.close()
 
 f1.close()
