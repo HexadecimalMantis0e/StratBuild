3c0a783f20bc5aaae32ae35cfe958a09cf3ae96d
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2021-08-27 21:49:12 -0400

    Update for Python3 and refactor

--- StratBuild.py
+++ StratBuild.py
@@ -1,145 +1,130 @@
 import os
 import struct
 import argparse
+import io
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d","--description", nargs='?', help="Strat description")
-parser.add_argument("-s","--signature", nargs='?', help="Developer signature")
-parser.add_argument("-p","--path", nargs='?', help="Build path")
-parser.add_argument("C00", help="Segment 00")
-parser.add_argument("C01", help="Segment 01")
-parser.add_argument("-U0","--UC00", nargs='?', help="Uncompressed segment 00")
-parser.add_argument("-U1","--UC01", nargs='?', help="Uncompressed segment 01")
-parser.add_argument("-pc","--pcstrat", action= "store_true", help="Do not attach uncompressed segments to strat")
+parser.add_argument("-d", "--description", nargs = '?', help = "Strat WAD description")
+parser.add_argument("-s", "--signature", nargs = '?', help = "Strat WAD signature")
+parser.add_argument("-p", "--path", nargs = '?', help = "Strat WAD build path")
+parser.add_argument("C00", help = "Compressed segment 00")
+parser.add_argument("C01", help = "Compressed segment 01")
+parser.add_argument("-U0", "--UC00", nargs = '?', help = "Uncompressed segment 00")
+parser.add_argument("-U1", "--UC01", nargs = '?', help = "Uncompressed segment 01")
+parser.add_argument("-pc", "--pcstrat", action = "store_true", help = "Build PC strat WAD")
 args = parser.parse_args()
 
-def getSize(filename):
-    f2 = open(filename,"rb")
-    f2.seek(0x00, os.SEEK_END)
-    size = f2.tell()
-    f2.close()
-    return size
+def padArgument(optionalArg, defaultValue, basePadding):
+    fb = io.BytesIO()
+    if optionalArg is not None:
+        fb.write(optionalArg.encode())
+        paddingSize = basePadding - len(optionalArg)
+        fb.write(bytearray([0x00]) * paddingSize)
+    else:
+        fb.write(defaultValue.encode())
+        paddingSize = basePadding - len(defaultValue)
+        fb.write(bytearray([0x00]) * paddingSize)
+    return fb
 
-def getLiteralSize(filesize):
-    literalSize = (filesize / 0x04) * (0x05)
+def getSize(filePointer):
+    filePointer.seek(0x00, os.SEEK_END)
+    fileSize = filePointer.tell()
+    filePointer.seek(0x00, os.SEEK_SET)
+    return fileSize
+
+def getLiteralSize(fileSize):
+    literalSize = (fileSize // 0x04) * 0x05
     return literalSize
 
-def literalComp(filename, filesize):
-    f2 = open(filename,"rb")
-    loopsize = (filesize / 0x04)
-    for i in range(0,loopsize):
-        literalBytes = struct.unpack('i', f2.read(4))[0]
-        f1.write(bytearray([0x03]))
-        f1.write(struct.pack("i", literalBytes))
-    f2.close()
+def literalComp(filePointer, fileSize):
+    fb = io.BytesIO()
+    loopCount = (fileSize // 0x04)
+    for i in range(0, loopCount):
+        literalBytes = struct.unpack('I', filePointer.read(4))[0]
+        fb.write(struct.pack('B', 0x03))
+        fb.write(struct.pack('I', literalBytes))
+    return fb
 
-def uncompHandle(filename, filesize):
-    uncompressed_alignment = 0x800
-    f2 = open(filename,"rb")
-    filebytes = f2.read(filesize)
-    data_offset = f1.tell()
-    new_offset = (data_offset + uncompressed_alignment - 1) / uncompressed_alignment * uncompressed_alignment
-    paddsize = new_offset - data_offset
-    f1.write(bytearray([0]*paddsize))
-    f1.write(filebytes)
-    f2.close()
-    return paddsize
+def padUncomp(filePointer, fileSize, fileOffset):
+    fb = io.BytesIO()
+    uncompressedAlignment = 0x800
+    paddingSize = ((fileOffset + uncompressedAlignment - 0x01) // uncompressedAlignment * uncompressedAlignment) - fileOffset
+    fb.write(bytearray([0x00]) * paddingSize)
+    fb.write(filePointer.read(fileSize))
+    return fb
 
-f1 = open("out","wb")
+def main():
+    f0 = open("out", "wb")
+    print("Creating Strat WAD header...")
+    
+    f0.write("BIGB".encode())
+    f0.write(struct.pack('I', 0x180))
+    f0.write(struct.pack('I', 0x74))
+    f0.write(struct.pack('I', 0x01))
+    f0.write(padArgument(args.description, "<Strat Wad>", 0x40).getbuffer())
+    f0.write(padArgument(args.signature, "normal", 0x28).getbuffer())
 
-fullsize = 0x190
-
-print "Creating Strat WAD header"
-
-f1.write("BIGB")
-f1.write(struct.pack("i", 0x180))
-f1.write(struct.pack("i", 0x74))
-f1.write(struct.pack("i", 0x01))
-
-if args.description is not None:
-    f1.write(args.description)
-    descriptionPadding = 0x40 - len(args.description)
-    f1.write(bytearray([0])*descriptionPadding)
-else:
-    f1.write("<Strat Wad>")
-    f1.write(bytearray([0])*0x35)
-
-if args.signature is not None:
-    f1.write(args.signature)
-    signaturePadding = 0x28 - len(args.signature)
-    f1.write(bytearray([0])*signaturePadding)
-else:
-    f1.write("normal")
-    f1.write(bytearray([0])*0x22)
-
-
-if args.UC00 is not None:
-
-    sizeUC00 = getSize(args.UC00)
-    f1.write(struct.pack("i", sizeUC00))
-
-else:
-    f1.write(struct.pack("i", 0x00))
-
-if args.UC01 is not None:
-
-    sizeUC01 = getSize(args.UC01)
-    f1.write(struct.pack("i", sizeUC01))
-
-else:
-    f1.write(struct.pack("i", 0x00))
-
-size00 = getSize(args.C00)
-f1.write(struct.pack("i", size00))
-
-size01 = getSize(args.C01)
-f1.write(struct.pack("i", size01))
-
-literalSize00 = getLiteralSize(size00)
-literalSize01 = getLiteralSize(size01)
-
-f1.write(struct.pack("i", literalSize00))
-f1.write(struct.pack("i", literalSize01))
-
-fullsize += (literalSize00 + literalSize01)
-
-if args.path is not None:
-    f1.write(args.path)
-    pathPadding = 0x100 - len(args.path)
-    f1.write(bytearray([0])*pathPadding)
-
-else:
-    currentDirectory = os.getcwd()
-    f1.write(currentDirectory)
-    pathPadding = 0x100 - len(currentDirectory)
-    f1.write(bytearray([0])*pathPadding)
-
-print "Converting segment 00"
-literalComp(args.C00, size00)
-
-print "Converting segment 01"
-literalComp(args.C01, size01)
-
-if args.pcstrat == True:
-    print "Skipping attachment of uncompressed segments"
-
-else:
     if args.UC00 is not None:
-        print "Attaching uncompressed segment 00"
-        fullsize += sizeUC00 + uncompHandle(args.UC00, sizeUC00)
+        f1 = open(args.UC00, "rb")
+        sizeUC00 = getSize(f1)
+        f0.write(struct.pack('I', sizeUC00))
+        if args.pcstrat == True:
+            f1.close()
+    else:
+        f0.write(struct.pack('I', 0x00))
 
     if args.UC01 is not None:
-        print "Attaching uncompressed segment 01"
-        fullsize += sizeUC01 + uncompHandle(args.UC01, sizeUC01)
+        f2 = open(args.UC01, "rb")
+        sizeUC01 = getSize(f2)
+        f0.write(struct.pack('I', sizeUC01))
+        if args.pcstrat == True:
+            f2.close()
+    else:
+        f0.write(struct.pack('I', 0x00))
 
-# handle end padding
+    f3 = open(args.C00, "rb")
+    sizeC00 = getSize(f3)
+    f0.write(struct.pack('I', sizeC00))
+    f4 = open(args.C01, "rb")
+    sizeC01 = getSize(f4)
+    f0.write(struct.pack('I', sizeC01))
 
-print "Padding Strat WAD"
+    f0.write(struct.pack('I', getLiteralSize(sizeC00)))
+    f0.write(struct.pack('I', getLiteralSize(sizeC01)))
+    f0.write(padArgument(args.path, os.getcwd(), 0x100).getbuffer())
 
-while fullsize % 0x1000 != 0x00:
-    f1.write(bytearray([0]))
-    fullsize += 0x01
+    print("Converting segment 00...")
+    f0.write(literalComp(f3, sizeC00).getbuffer())
+    f3.close()
+    print("Converting segment 01...")
+    f0.write(literalComp(f4, sizeC01).getbuffer())
+    f4.close()
 
-print "Done!"
+    if args.pcstrat == True:
+        print("Skipping attachment of uncompressed segments...")
+    else:
+        if args.UC00 is not None:
+            dataOffset = f0.tell()
+            print("Attaching uncompressed segment 00...")
+            f0.write(padUncomp(f1, sizeUC00, dataOffset).getbuffer())
+            f1.close()
 
-f1.close()
+        if args.UC01 is not None:
+            dataOffset = f0.tell()
+            print("Attaching uncompressed segment 01...")
+            f0.write(padUncomp(f2, sizeUC01, dataOffset).getbuffer())
+            f2.close()
+
+    print("Padding Strat WAD...")
+    fullSize = getSize(f0)
+    f0.seek(0x00, os.SEEK_END)
+
+    while fullSize % 0x1000 != 0x00:
+        f0.write(struct.pack('B', 0x00))
+        fullSize += 0x01
+
+    print("Done!")
+    f0.close()
+
+if __name__ == "__main__":
+    main()
